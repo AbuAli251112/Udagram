21566ec5a58f80b3fe1723dd6c2e872cc1bed6a3
AbuAli251121 <abualininja@gmail.com>
2022-07-04 09:10:20 +0200

    update

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,3 +1,2 @@
-aws configure import --csv credentials.csv
 aws s3 cp --recursive --acl public-read ./www s3://myawsbucket251121/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket251121/
