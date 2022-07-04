96b74ee1d5f76a66d67fb70b539aa09dcd1c5d5a
AbuAli251121 <abualininja@gmail.com>
2022-07-04 11:39:30 +0200

    u

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,7 +1,3 @@
-sudo apt-get -y -qq update
-sudo apt-get install python3-pip python-dev build-essential
-sudo pip install --upgrade setuptools
-sudo pip install awsebcli --upgrade
 eb init myprojectv3 --region us-east-1
 eb use Myprojectv3-env
 eb deploy Myprojectv3-env
