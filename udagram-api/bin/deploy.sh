9e30b51c204c9dabcf2a4da4b7f69a9dbc29fe33
AbuAli251121 <abualininja@gmail.com>
2022-07-05 07:07:59 +0200

    U

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
 eb init myprojectv3 --region us-east-1 -p node.js
-eb create myprojectv3-env --service-role
+eb create myprojectv3-env --service-role aws-elasticbeanstalk-service-role
 eb use myprojectv3-env
 eb deploy myprojectv3-env
