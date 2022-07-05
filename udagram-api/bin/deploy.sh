e4c6b44407ca544d6b52d1917898f1c415b69c6b
AbuAli251121 <abualininja@gmail.com>
2022-07-05 09:27:53 +0200

    U

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb init myprojectv3 --region us-east-1 -p node.js
-eb use Myprojectv3-env
-eb deploy Myprojectv3-env
+cd ./www
+aws elasticbeanstalk create-application-version --application-name MyApp --version-label <<pipeline.git.revision>> --region us-east-1 --source-bundle S3Bucket="myawsbucket251121",S3Key="Archive.zip"
+aws elasticbeanstalk update-environment --application-name MyApp --environment-name MyApp --version-label <<pipeline.git.revision>>
