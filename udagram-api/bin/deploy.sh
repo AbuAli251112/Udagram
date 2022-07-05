c9f157c138f973f2838f43140c791b40aa67d5a8
AbuAli251121 <abualininja@gmail.com>
2022-07-05 10:09:21 +0200

    U

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,7 +1,3 @@
-eb init myprojectv3 --region us-east-1 -p node.js
-touch CodeDeployDemo-Trust.json
-echo "{"Version": "2012-10-17","Statement": [{"Sid": "","Effect": "Allow","Principal": {"Service": ["codedeploy.amazonaws.com"]},"Action": "sts:AssumeRole"}]}" > CodeDeployDemo-Trust.json
-aws iam create-role --role-name CodeDeployServiceRole --assume-role-policy-document file://CodeDeployDemo-Trust.json
-eb create myprojectv3-env
-eb use myprojectv3-env
-eb deploy myprojectv3-env
+eb init udagram-api --region us-east-1 --platform node.js
+eb use udagram-api-env
+eb deploy udagram-api-env
