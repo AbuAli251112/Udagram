952702b332db49526f941256a643b956218a0662
AbuAli251121 <abualininja@gmail.com>
2022-07-08 15:50:12 +0200

    u

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws elasticbeanstalk create-application-version --application-name $EB_APP --version-label <<pipeline.git.revision>> --source-bundle S3Bucket="$EB_BUCKET",S3Key="Archive.zip"
-aws elasticbeanstalk update-environment --application-name $EB_APP --environment-name $EB_ENV --version-label <<pipeline.git.revision>>
+aws elasticbeanstalk create-application-version --application-name udagram-api --version-label <<pipeline.git.revision>> --source-bundle S3Bucket="$EB_BUCKET",S3Key="Archive.zip"
+aws elasticbeanstalk update-environment --application-name udagram-api --environment-name udagramapi-env --version-label <<pipeline.git.revision>>
