78cdb688b7b59a21a95350a935c19b42851edae9
AbuAli251121 <abualininja@gmail.com>
2022-07-08 02:14:19 +0200

    U

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,3 @@
 eb init udagram-api --region us-east-1 --platform node.js
 eb use udagramapi-env
-aws s3api list-buckets --query "Owner.ID"
-aws s3api list-objects --bucket myawsbucket251121 --prefix myawsbucket251121/
-aws s3api put-object-acl --bucket myawsbucket251121 --key myawsbucket251121/ --acl bucket-owner-full-control
 eb deploy udagramapi-env
